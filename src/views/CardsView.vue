fd81ddc62a3dff836bf9c35cb5b626a08afa92b5
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-06-26 18:33:45 -0400

    Cards and subscriptions list

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -1,5 +1,38 @@
 <template>
-  Cartões
+  <v-table density="compact">
+    <thead>
+      <tr>
+        <th class="text-left">
+          #ID
+        </th>
+        <th class="text-left">
+          Informações
+        </th>
+        <th class="text-center">
+          Ações
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="card in cards" :key="card.id">
+        <td>{{ card.id }}</td>
+        <td class="py-4">
+          <v-card elevation="0">
+            <v-card-text>
+              <div>{{ card.number }}</div>
+              <div>{{ card.expiration_date }} - {{ card.brand.toUpperCase() }}</div>
+            </v-card-text>
+          </v-card>
+        </td>
+        <td class="text-center">
+          <v-btn text="Editar" variant="outlined" class="text-none mx-1" color="primary"
+            size="small"></v-btn>
+          <v-btn text="Excluir" variant="outlined" class="text-none mx-1" color="red"
+            size="small"></v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
 <script>
@@ -7,6 +40,36 @@
 import { useAppStore } from '@/store/app';
 
 export default {
+  data() {
+    return {
+      cards: [
+        {
+          id: 4,
+          number: '**** **** **** 8989',
+          brand: 'visa',
+          expiration_date: '0226'
+        },
+        {
+          id: 3,
+          number: '**** **** **** 8989',
+          brand: 'visa',
+          expiration_date: '0226'
+        },
+        {
+          id: 2,
+          number: '**** **** **** 8989',
+          brand: 'visa',
+          expiration_date: '0226'
+        },
+        {
+          id: 1,
+          number: '**** **** **** 8989',
+          brand: 'mastercard',
+          expiration_date: '0226'
+        }
+      ]
+    };
+  },
   created() {
     useAppStore().updateBreadcrumbs([
       {
--- src/views/SubscriptionsView.vue
+++ src/views/SubscriptionsView.vue
@@ -1,5 +1,38 @@
 <template>
-  Inscrições
+  <v-table fixed-header>
+    <thead>
+      <tr>
+        <th class="text-left">
+          #ID
+        </th>
+        <th class="text-left">
+          Período
+        </th>
+        <th class="text-left">
+          Status
+        </th>
+        <th class="text-center">
+          Ações
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="subscription in subscriptions" :key="subscription.id">
+        <td>{{ subscription.id }}</td>
+        <td>
+          <div>{{ subscription.starts_in }} <span class="font-weight-bold">à</span> {{ subscription.ends_in }}</div>
+        </td>
+        <td>
+          <v-chip :color="allowedSubscriptionStatusConfig[subscription.status].color">
+            {{ allowedSubscriptionStatusConfig[subscription.status].text }}
+          </v-chip>
+        </td>
+        <td class="text-center">
+          <v-btn text="Ver mais" variant="plain" prepend-icon="mdi:eye-outline" class="text-none"></v-btn>
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
 <script>
@@ -7,6 +40,54 @@
 import { useAppStore } from '@/store/app';
 
 export default {
+  data() {
+    return {
+      allowedSubscriptionStatusConfig: {
+        active: {
+          text: 'Ativo',
+          color: 'green'
+        },
+        pending: {
+          text: 'Pendente',
+          color: 'secondary'
+        },
+        ended: {
+          text: 'Finalizado',
+          color: 'orange'
+        },
+        canceled: {
+          text: 'canceled',
+          color: 'red'
+        },
+      },
+      subscriptions: [
+        {
+          id: 4,
+          starts_in: '2023-06-01',
+          ends_in: '2023-12-01',
+          status: 'active'
+        },
+        {
+          id: 3,
+          starts_in: '2023-01-01',
+          ends_in: '2023-06-01',
+          status: 'pending'
+        },
+        {
+          id: 2,
+          starts_in: '2023-01-01',
+          ends_in: '2023-06-01',
+          status: 'canceled'
+        },
+        {
+          id: 1,
+          starts_in: '2023-01-01',
+          ends_in: '2023-06-01',
+          status: 'ended'
+        }
+      ]
+    };
+  },
   created() {
     useAppStore().updateBreadcrumbs([
       {
