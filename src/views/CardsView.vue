4a1ed0cf69dd9bcf9835772d251a701d99248afb
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-06-26 19:44:03 -0400

    Delete item list

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -25,10 +25,10 @@
           </v-card>
         </td>
         <td class="text-center">
-          <v-btn text="Editar" variant="outlined" class="text-none mx-1" color="primary"
-            size="small"></v-btn>
-          <v-btn text="Excluir" variant="outlined" class="text-none mx-1" color="red"
-            size="small"></v-btn>
+          <v-btn text="Editar" variant="outlined" class="text-none mx-1" color="primary" size="small"></v-btn>
+          <confirmation :data-identificator="card.id" text="Excluir" color="red" size="small" variant="outlined"
+            :dialog-title="'Tem certeza de que deseja excluir o cartão final ' + card.last_digits + '?'"
+            :confirm-callback="methodDeleteCardConfirmed"></confirmation>
         </td>
       </tr>
     </tbody>
@@ -38,34 +38,40 @@
 <script>
 
 import { useAppStore } from '@/store/app';
+import Confirmation from '@/components/Confirmation.vue';
 
 export default {
+  components: { Confirmation },
   data() {
     return {
       cards: [
         {
           id: 4,
-          number: '**** **** **** 8989',
-          brand: 'visa',
-          expiration_date: '0226'
+          number: "**** **** **** 8989",
+          last_digits: 8989,
+          brand: "visa",
+          expiration_date: "0226"
         },
         {
           id: 3,
-          number: '**** **** **** 8989',
-          brand: 'visa',
-          expiration_date: '0226'
+          number: "**** **** **** 8989",
+          last_digits: 8989,
+          brand: "visa",
+          expiration_date: "0226"
         },
         {
           id: 2,
-          number: '**** **** **** 8989',
-          brand: 'visa',
-          expiration_date: '0226'
+          number: "**** **** **** 8989",
+          last_digits: 8989,
+          brand: "visa",
+          expiration_date: "0226"
         },
         {
           id: 1,
-          number: '**** **** **** 8989',
-          brand: 'mastercard',
-          expiration_date: '0226'
+          number: "**** **** **** 8989",
+          last_digits: 8989,
+          brand: "mastercard",
+          expiration_date: "0226"
         }
       ]
     };
@@ -73,16 +79,26 @@ export default {
   created() {
     useAppStore().updateBreadcrumbs([
       {
-        title: 'Home',
+        title: "Home",
         disabled: false,
-        to: { name: 'app.home' }
+        to: { name: "app.home" }
       },
       {
-        title: 'Cartões',
+        title: "Cartões",
         disabled: true,
-        to: { name: 'app.cards' }
+        to: { name: "app.cards" }
       }
     ]);
+  },
+  methods: {
+    methodDeleteCardConfirmed(event) {
+      let target = event.target;
+      let index = this.cards.findIndex((card) => {
+        return card.id == target.getAttribute("data-identificator");
+      });
+
+      this.cards.splice(index, 1);
+    }
   }
 }
 
