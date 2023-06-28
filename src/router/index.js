c7a2845d8aa3205b2c02a16e94beffec2530ca16
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-06-28 09:38:57 -0400

    Rearranging

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import middleware from '../services/middleware'
 const routes = [
   {
     path: '/',
+    beforeEnter: middleware.redirectIfUnauthenticated,
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
@@ -13,31 +14,27 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        beforeEnter: middleware.redirectIfUnauthenticated,
+        // beforeEnter: middleware.redirectIfUnauthenticated,
         component: () => import(/* webpackChunkName: "home" */ '@/views/HomeView.vue'),
       },
       {
         path: '/assinaturas',
         name: 'app.subscriptions',
-        beforeEnter: middleware.redirectIfUnauthenticated,
-        component: () => import('@/views/SubscriptionsView.vue')
+        component: () => import('@/views/Subscriptions/SubscriptionsView.vue')
       },
       {
         path: '/assinaturas/nova-assinatura',
         name: 'app.subscriptions.new',
-        beforeEnter: middleware.redirectIfUnauthenticated,
-        component: () => import('@/views/SubscriptionsNewView.vue')
+        component: () => import('@/views/Subscriptions/SubscribeView.vue')
       },
       {
         path: '/cartoes',
         name: 'app.cards',
-        beforeEnter: middleware.redirectIfUnauthenticated,
-        component: () => import('@/views/CardsView.vue')
+        component: () => import('@/views/Cards/CardsView.vue')
       },
       {
         path: '/perfil',
         name: 'app.profile',
-        beforeEnter: middleware.redirectIfUnauthenticated,
         component: () => import('@/views/ProfileView.vue')
       }
     ],
