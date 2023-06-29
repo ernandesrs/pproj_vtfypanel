df854506497f50eea2e0dc7842ef4527416059d9
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-06-29 09:07:11 -0400

    ListGroupElem pagination

--- src/components/ListGroupElem.vue
+++ src/components/ListGroupElem.vue
@@ -1,38 +1,53 @@
 <template>
-    <v-table v-if="items.length">
-        <thead v-if="heads.length">
-            <tr>
-                <th class="font-weight-bold">ID</th>
-                <th class="font-weight-bold">Informações</th>
-                <th v-if="computedHasAction" class="font-weight-bold">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="item, index in items">
-                <td v-if="item?.id" class="text-right">
-                    {{ item.id }}
-                </td>
-                <td class="w-100 py-6">
-                    <slot :item="item" :index="index" />
-                </td>
-                <td v-if="computedHasAction">
-                    <div class="d-flex align-center">
-                        <!-- actions button -->
-                        <v-btn v-if="actionShow" @click.stop="methodShowItem" text="Ver" prepend-icon="mdi-eye-outline"
-                            size="small" color="secondary" class="ma-1" :data-identificator="item?.id ?? index" />
-                        <v-btn v-if="actionEdit" @click.stop="methodEditItem" text="Editar"
-                            prepend-icon="mdi-square-edit-outline" size="small" color="primary" class="ma-1"
-                            :data-identificator="item?.id ?? index" />
-                        <confirmation-button v-if="actionDelete" text="Excluir" icon="mdi-delete-outline" size="small"
-                            color="red" variant="outlined" :dialog-title="actionDeleteDialogTitle ?? 'Confirmar exclusão?'"
-                            :data-identificator="item?.id ?? index" :confirm-callback="computedGetConfirmCallback"
-                            :confirm-route="computedGetConfirmRoute" />
-                        <!-- /actions button -->
+    <div v-if="listItems.length">
+        <v-table>
+            <thead>
+                <tr>
+                    <th class="font-weight-bold">ID</th>
+                    <th class="font-weight-bold">Informações</th>
+                    <th v-if="computedHasAction" class="font-weight-bold">Ações</th>
+                </tr>
+            </thead>
+            <tbody style="position: relative;">
+
+                <div v-if="loadingList"
+                    style="position: absolute; bottom: 0; right: 0; width: 100%; height: 100%; background-color: rgba(246, 246, 246, 0.75); z-index: 1000; color: green;">
+                    <div class="d-flex justify-center align-center w-100 h-100">
+                        <loading-elem></loading-elem>
                     </div>
-                </td>
-            </tr>
-        </tbody>
-    </v-table>
+                </div>
+
+                <tr v-for="item, index in items">
+                    <td v-if="item?.id" class="text-right">
+                        {{ item.id }}
+                    </td>
+                    <td class="w-100 py-6">
+                        <slot :item="item" :index="index" />
+                    </td>
+                    <td v-if="computedHasAction">
+                        <div class="d-flex align-center">
+                            <!-- actions button -->
+                            <v-btn v-if="actionShow" @click.stop="methodShowItem" text="Ver" prepend-icon="mdi-eye-outline"
+                                size="small" color="secondary" class="ma-1" :data-identificator="item?.id ?? index" />
+                            <v-btn v-if="actionEdit" @click.stop="methodEditItem" text="Editar"
+                                prepend-icon="mdi-square-edit-outline" size="small" color="primary" class="ma-1"
+                                :data-identificator="item?.id ?? index" />
+                            <confirmation-button v-if="actionDelete" text="Excluir" icon="mdi-delete-outline" size="small"
+                                color="red" variant="outlined"
+                                :dialog-title="actionDeleteDialogTitle ?? 'Confirmar exclusão?'"
+                                :data-identificator="item?.id ?? index" :confirm-callback="computedGetConfirmCallback"
+                                :confirm-route="computedGetConfirmRoute" />
+                            <!-- /actions button -->
+                        </div>
+                    </td>
+                </tr>
+
+            </tbody>
+        </v-table>
+
+        <v-pagination v-if="listPages.length" :length="listPages.length - 2" :total-visible="5"
+            @update:model-value="methodPaginationUpdateModelValue"></v-pagination>
+    </div>
     <v-card v-else>
         <v-card-text>
             <p class="text-h7 text-lg-h6 text-center text-grey-darken-1">
@@ -45,22 +60,30 @@
 <script>
 
 import ConfirmationButton from './ConfirmationButton.vue';
+import LoadingElem from './LoadingElem.vue';
 
 export default {
-    components: { ConfirmationButton },
+    components: { ConfirmationButton, LoadingElem },
     data() {
-        return {};
+        return {
+            listItems: [],
+            listPages: [],
+            loadingList: false
+        };
+    },
+    events: {
+        changePage: null
     },
     props: {
         emptyListText: {
             type: String,
             default: null
         },
-        heads: {
-            type: Array,
-            default: ['ID', 'Informações']
-        },
         items: {
+            type: Array,
+            default: []
+        },
+        pages: {
             type: Array,
             default: []
         },
@@ -79,6 +102,25 @@ export default {
         actionDeleteDialogTitle: {
             type: String,
             default: null
+        }
+    },
+    watch: {
+        items: {
+            deep: true,
+            immediate: true,
+            handler(nv, ov) {
+                this.listItems = nv;
+                if (this.loadingList) {
+                    this.loadingList = false;
+                }
+            }
+        },
+        pages: {
+            deep: true,
+            immediate: true,
+            handler(nv, ov) {
+                this.listPages = nv;
+            }
         }
     },
     computed: {
@@ -114,6 +156,10 @@ export default {
                     window.location.href = action
                     break;
             }
+        },
+        methodPaginationUpdateModelValue(currentPage) {
+            this.loadingList = true;
+            this.$emit('changePage', { page: currentPage, url: this.listPages[currentPage] });
         }
     }
 }
