fd1256022747eff37a5232a77280092a0e599b66
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-06-30 08:41:07 -0400

    Verify account alert. Resend verification link.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,27 +2,19 @@
   <loading-elem v-if="loadingContent" />
 
   <template v-else>
-    <template v-if="hasActiveSubscription">
-      <v-sheet width="100%" border rounded elevation="4"
-        style="background-color: rgba(164, 240, 226, 0.262); color: #585858;">
-        <v-container class="text-center">
-          <h1 class="text-center text-h5 font-weight-bold" style="color: rgb(72, 112, 72);">Você é PRO!</h1>
-          <p class="text-center text-h7 mb-5"><v-btn color="primary" variant="outlined"
-              class="text-none text-subtitle-1">Clique aqui</v-btn> e saiba como desfrutar de nossos recursos especiais
-            para
-            quem é PRO!</p>
-        </v-container>
-      </v-sheet>
-    </template>
-    <template v-else>
-      <v-sheet width="100%" border rounded elevation="4" style="background-color: rgb(233, 239, 242); color: #585858;">
-        <v-container class="text-center">
-          <h1 class="text-center text-h5 font-weight-bold">Parece que você não tem uma assinatura</h1>
-          <p class="text-center text-h7 mb-5">Nosso plano PRO possui diversos recursos que farão você querer mais!</p>
-          <v-btn append-icon="mdi-arrow-right" text="Veja e assine" size="large" color="primary" :to="{ name: 'app.subscriptions.new' }"></v-btn>
-        </v-container>
-      </v-sheet>
-    </template>
+    <v-sheet class="py-5">
+      <v-row>
+        <v-col v-if="!computedUserApp.email_verified_at" cols="12">
+          <v-alert :title="verification.resended ? 'Novo link enviado!' : 'Verifique sua conta!'" variant="outlined"
+            :type="verification.resended ? 'success' : 'warning'" border="top">
+            Um link de verificação foi enviado para o email cadastrado, acesse-o e clique no link para confirmar. <span
+              v-if="!verification.resended">Ou
+              <v-btn @click.stop="methodResendLink" variant="outlined" size="small" class="mx-1"
+                :loading="verification.verificationLinkResending">clique aqui</v-btn> para reenviar.</span>
+          </v-alert>
+        </v-col>
+      </v-row>
+    </v-sheet>
   </template>
 </template>
 
@@ -36,8 +28,12 @@ export default {
   components: { LoadingElem },
   data() {
     return {
+      loadingContent: true,
       hasActiveSubscription: false,
-      loadingContent: true
+      verification: {
+        resended: false,
+        verificationLinkResending: false
+      }
     };
   },
   created() {
@@ -61,6 +57,26 @@ export default {
         this.loadingContent = false;
       }
     });
+  },
+  methods: {
+    methodResendLink() {
+      this.verification.verificationLinkResending = true;
+      axios.req({
+        action: '/auth/resend-verification',
+        method: 'get',
+        success: (resp) => {
+          this.verification.resended = true;
+        },
+        finally: () => {
+          this.verification.verificationLinkResending = false;
+        }
+      });
+    }
+  },
+  computed: {
+    computedUserApp() {
+      return useAppStore().appUser;
+    }
   }
 }
 </script>
