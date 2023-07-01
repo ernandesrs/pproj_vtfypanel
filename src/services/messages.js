1007e3d29b726247db21c93c4502685cab69e060
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-07-01 11:19:41 -0400

    [All] Erro messages

--- src/services/messages.js
+++ src/services/messages.js
@@ -2,13 +2,27 @@ const errors = {
     // all
     InvalidDataException: 'Um ou mais dados informados são inválidos.',
     NotFoundException: 'Recurso não encontrado.',
+    UnauthorizedException: 'Você não possui autorização.',
+    UnauthorizedActionException: 'Você não possui autorização para esta ação.',
+    NotHaveAdminPanelAcessException: 'Não possui acesso ao painel administrativo.',
 
     // auth
     LoginFailException: 'E-mail e/ou senha inválidos ou não existem.',
     VerificationTokenInvalidException: 'Token de verificação é inválido. Acesse sua conta e solicite um novo link.',
+    UpdatePasswordTokenInvalidException: 'Token de recuperação é inválido.',
+
+    // admin
+    HasDependentsException: 'Há outros recursos que dependem deste.',
 
     // dash
-    InvalidCreditCardException: 'Cartão de crédito inválido.'
+    InvalidCreditCardException: 'Cartão de crédito inválido.',
+    HasActiveSubscriptionException: 'Você tem uma assinatura ativa.',
+    RefundPaymentFailException: 'Falha ao estornar.',
+    RefundedPaymentException: 'Pagamento estornado.',
+    
+    RefusedPaymentException: 'Pagamento recusado.',
+    PaymentFailException: 'Falha no pagamento.',
+    ChargebackPaymentException: 'Pagamento retornado.'
 };
 
 export default {
